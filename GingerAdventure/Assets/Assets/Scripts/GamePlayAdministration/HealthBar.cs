2faf29873ca5520d7a89545c3207f5e74d967a8a
faAanya <108868725+faAanya@users.noreply.github.com>
2023-11-07 21:18:59 +0300

    useless code deleted

--- GingerAdventure/Assets/Assets/Scripts/GamePlayAdministration/HealthBar.cs
+++ GingerAdventure/Assets/Assets/Scripts/GamePlayAdministration/HealthBar.cs
@@ -8,14 +8,10 @@ public class HealthBar : MonoBehaviour
     public Slider slider;
   
 
-    public void SetMaxField(int health)
-    {
-        slider.maxValue = health;
-        slider.value = health;
-    }
-
     public void SetHealth(int health)
     {
         slider.value = health;
     }
+
+ 
 }
